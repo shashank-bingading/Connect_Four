932b9f042a5b317a792eb08cd8d8eddcd67a976a
Shashank Singh <shashanksinghwork88@gmail.com>
2025-10-05 00:58:46 +0530

    Add turn based system and interactivity

--- Frontend/Connect_Four/src/Components/board.jsx
+++ Frontend/Connect_Four/src/Components/board.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import '../styles/board.css'
+import { useState } from 'react';
 
 const Board = () => {
     const rows = 6;
     const cols = 7;
+    const [board, setBoard] = useState(
+        Array(rows).fill(null).map(() => Array(cols).fill(null))
+    );
+    const [currentPlayer, setCurrentPlayer] = useState("R");
+
+    const handleClickCell = (colIndex) => {
+        for(let row = rows - 1; row >= 0; row--){
+            if(!board[row][colIndex]){
+                const newBoard = board.map(ro => [...ro]);
+                newBoard[row][colIndex] = currentPlayer;
+                setBoard(newBoard); 
+                setCurrentPlayer(currentPlayer === "R" ? "Y" : "R");
+                break;
+            }};
+
+   
+    
   return (
     <div style={{justifyContent:'center',alignItems:'center'}}>
         <h1>Connect Four Board</h1>
@@ -11,7 +29,11 @@ const Board = () => {
         {Array.from({length: rows}).fill(0).map((a,rowindex)=>(
             <div key={rowindex} className='row'>
                 {Array(cols).fill(0).map((b,colindex)=>(
-                    <div key={colindex} className='col'></div>
+                    <div key={colindex} className='col'
+                    onClick={()=>handleClickCell(colindex)}>
+                        {b == "R" && <div className='red-disc'></div>}
+                        {b == "Y" && <div className='yellow-disc'></div>}
+                    </div>
                 ))}
             </div>
         ))}
@@ -19,6 +41,6 @@ const Board = () => {
       
     </div>
   )
-}
+}}
 
 export default Board

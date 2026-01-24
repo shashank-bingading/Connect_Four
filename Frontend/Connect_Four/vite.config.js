e7fc4f71995add110b8e65aee2d500738bf2cf6a
Shashank Singh <shashanksinghwork88@gmail.com>
2026-01-24 17:00:45 +0530

    UPDATE vite.config.js base

--- Frontend/Connect_Four/vite.config.js
+++ Frontend/Connect_Four/vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: "/Connect_Four/",
+  base: "/",
 });
